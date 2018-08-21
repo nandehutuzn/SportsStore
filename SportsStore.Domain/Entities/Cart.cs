2d4dcdb38b6b626f450dd78e0b8921e8fbce9216
120902259 <120902259@qq.com>
2018-08-21 21:41:32 +0800

    向购物车添加商品

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> _lineCollection = new List<CartLine>();
+
+        /// <summary>
+        /// 添加商品
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="quantity"></param>
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = _lineCollection.Where(p => p.Product.ProductID == product.ProductID).FirstOrDefault();
+            if (line == null)
+                _lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            else
+                line.Quantity += quantity;
+        }
+
+        /// <summary>
+        /// 移除商品
+        /// </summary>
+        /// <param name="product"></param>
+        public void RemoveLine(Product product)
+        {
+            _lineCollection.RemoveAll(p => p.Product.ProductID == product.ProductID);
+        }
+
+        /// <summary>
+        /// 计算商品总价
+        /// </summary>
+        /// <returns></returns>
+        public decimal ComputeTotalValue()
+        {
+            return _lineCollection.Sum(p => p.Product.Price * p.Quantity);
+        }
+
+        /// <summary>
+        /// 清空购物车
+        /// </summary>
+        public void Clear()
+        {
+            _lineCollection.Clear();
+        }
+
+        /// <summary>
+        /// 购物车所有商品
+        /// </summary>
+        public IEnumerable<CartLine> Lines {
+            get { return _lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        public void Can_Add_New_Lines()
+        {
+            Product p1 = new Product() { ProductID = 1, Name = "p1", Price = 5 };
+            Product p2 = new Product() { ProductID = 2, Name = "p2", Price = 8 };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            CartLine[] results = target.Lines.ToArray();
+
+            //断言
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0], p1);
+            Assert.AreEqual(results[1], p2);
+        }
+    }
+}
