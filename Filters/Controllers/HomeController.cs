551b148a9b986e27d031146a6d26ad5cee48946b
120902259 <120902259@qq.com>
2018-08-24 21:59:01 +0800

    自定义过滤器

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        [Authorize(Users = "admin")]
         public string Index()
         {
             return "This is the Index action on the Home controller";
---
+++ Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class CustomAuthAttribute : AuthorizeAttribute
+    {
+        private bool _localAllowed;
+
+        public CustomAuthAttribute(bool allowedParam)
+        {
+            _localAllowed = allowedParam;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+                return _localAllowed;
+            else
+                return true;
+        }
+    }
+}
