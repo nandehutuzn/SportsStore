76b55d8332b7c364973c6ea2d5435c0689d3e8c9
120902259 <120902259@qq.com>
2018-08-25 19:13:25 +0800

    辅助器方法（内敛方法、外部辅助器）

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new[] { "New York", "London", "Paris" };
+            string message = "This is an Html element: <input>";
+            return View((object)message);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ HelperMethods/Infrastructure/CustomHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelper
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach (string str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string msg)
+        {
+            string encodeMessage = html.Encode(msg);
+            string result = $"This is the message: <p>{encodeMessage}</p>";
+            return new MvcHtmlString(result);
+        }
+    }
+}
